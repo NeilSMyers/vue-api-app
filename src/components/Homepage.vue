a54f8a26f942b49071444c00fd0230dc4722a79a
neilmyers <mmyersneil@gmail.com>
2018-12-12 16:24:30 -0700

    homepage and searchresults components skeleton

---
+++ src/components/Homepage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Homepage</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Homepage',
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Homepage from '@/components/Homepage'
+import SearchResults from '@/components/SearchResults'
 
 Vue.use(Router)
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Homepage',
+      component: Homepage
+    },
+    {
+      path: '/search-results',
+      name: 'SearchResults',
+      component: SearchResults
     }
   ]
 })
