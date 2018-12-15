d43a9ff7495776d2190300252a5a3c2362337657
neilmyers <mmyersneil@gmail.com>
2018-12-14 17:09:58 -0700

    search results api connection

--- src/components/SearchResults.vue
+++ src/components/SearchResults.vue
@@ -1,21 +1,39 @@
 <template>
   <div>
     <h1>Search Results</h1>
-    <h2>{{boom}}</h2>
+    <div v-for="result in results" :key=result.id>
+      <h1>{{ result.title }}</h1>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'SearchResults',
   data() {
     return {
       query: null,
+      results: null
     }
   },
   beforeMount() {
     this.query = this.$route.params.query
-  }  
+    this.getResults(this.query)
+  },
+  methods: {
+    getResults(q) {
+      axios
+        .get("https://api.dailysmarty.com/search", { params: { q } })
+        .then(response => {
+          console.log(response.data.posts)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  }
 }
 </script>
 
