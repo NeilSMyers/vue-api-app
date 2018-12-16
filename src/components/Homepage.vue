94298d62c25587a04e96b17925a6ee43de91c9a3
neilmyers <mmyersneil@gmail.com>
2018-12-15 23:24:24 -0700

    html and styles for both components

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -1,12 +1,35 @@
 <template>
-  <div>
-    <h1>Homepage</h1>
 
-    <input type="text" @keyup.enter="submitQuery">
-    <div v-for="post in recentPosts" :key="post.id"> 
-      {{post.title}}
+   <div class="container container-homepage">
+        <div class="homepage-logo">
+            <img src="@/assets/ds_circle_logo.png" alt="">
+        </div>
+
+        <div class="search-bar homepage-search-bar">
+            <input type="text" placeholder="&#xf002; Search DailySmarty" @keyup.enter="submitQuery">
+            <p>Press return to search</p>
+        </div>
+
+        <div class="recent-posts-wrapper">
+            <div class="recent-posts-heading">
+                Recent Posts
+            </div>
+
+            <div class="recent-posts">
+                <div v-for="post in recentPosts" :key="post.id" class="recent-post">
+                    <div class="recent-post-title">
+                        <a :href="post.url_for_post" target="_blank">
+                          {{ post.title }}
+                        </a>
+                    </div>
+                    <div class="recent-post-category">
+                        {{ post.associated_topics[0] }}
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
+
 </template>
 
 <script>
@@ -42,6 +65,6 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
 
 </style>
